009e3eed60ed722150806d159b833cce03c94285
alfiephillips <thealfiephillips@gmail.com>
2023-12-15 16:56:09 +0000

    init: add usable data and read csv data

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-for i in range(20):
-    print("Dog")
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+attributes = ["G1", "G2", "G3", "studytime", "failures", "absences", "health"]
+
+data = (pd.read_csv("data.csv", sep=";"))[attributes]
+predict = "G3"
+
+x = np.array(data.drop([predict], 1))
